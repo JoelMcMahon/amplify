27f810ea802f617dc5165ff3a898866c0590889b
emilyb93 <emilybennett93@hotmail.com>
2021-11-01 16:45:07 +0000

    Map now zooms in to closest markers

--- src/Hooks/useMarkers.js
+++ src/Hooks/useMarkers.js
@@ -1,80 +1,128 @@
-import React, { useState, useEffect } from 'react'
-import {Marker} from "react-native-maps"
-import firebase from 'firebase'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-const geofire = require('geofire-common')
-
+import React, { useState, useEffect } from "react";
+import { Marker } from "react-native-maps";
+import firebase from "firebase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const geofire = require("geofire-common");
 
 export default function useMarkers() {
-    const [mapToggle, setMapToggle] = useState(true)
-    const [markerArray, setMarkerArray] = useState([])
+  const [mapToggle, setMapToggle] = useState(true);
+  const [markerArray, setMarkerArray] = useState([]);
 
-    const db = firebase.firestore()
+  const db = firebase.firestore();
 
-    const retrieveMarkers = async ()=>{
-        const lastPosition = await JSON.parse(await AsyncStorage.getItem('lastKnownPosition'))
-        console.log(lastPosition, "mapview")
-        const lastLat = lastPosition.coords.latitude
-        const lastLong = lastPosition.coords.longitude
-        const center = [lastLat, lastLong]
-        const radiusInM = 10 * 1000
-    
-    
-        const bounds = geofire.geohashQueryBounds(center, radiusInM)
-        const promises = []
-        for (const b of bounds){
-          const q = db.collection('ads')
-          .orderBy('geohash')
-          .startAt(b[0])
-          .endAt(b[1])
-          promises.push(q.get())
-        }
-    
-        Promise.all(promises).then((snapshots)=>{
-          const matchingDocs = []
-    
-          for(const snap of snapshots){
-            for (const doc of snap.docs){
-              const lat= doc.get('lat')
-              const long = doc.get('long')
-    
-              const distanceInKm = geofire.distanceBetween([lat, long], center)
-              const distanceInM = distanceInKm * 1000
-              if(distanceInM <= radiusInM){
-                matchingDocs.push(doc)
-              }
+  const retrieveMarkers = async () => {
+    const lastPosition = await JSON.parse(
+      await AsyncStorage.getItem("lastKnownPosition")
+    );
+    console.log(lastPosition, "mapview");
+    const lastLat = lastPosition.coords.latitude;
+    const lastLong = lastPosition.coords.longitude;
+    const center = [lastLat, lastLong];
+    const radiusInM = 10 * 1000;
+
+    const bounds = geofire.geohashQueryBounds(center, radiusInM);
+    const promises = [];
+    for (const b of bounds) {
+      const q = db
+        .collection("ads")
+        .orderBy("geohash")
+        .startAt(b[0])
+        .endAt(b[1]);
+      promises.push(q.get());
+    }
+
+    Promise.all(promises)
+      .then((snapshots) => {
+        const matchingDocs = [];
+
+        for (const snap of snapshots) {
+          for (const doc of snap.docs) {
+            const lat = doc.get("lat");
+            const long = doc.get("long");
+
+            const distanceInKm = geofire.distanceBetween([lat, long], center);
+            const distanceInM = distanceInKm * 1000;
+            if (distanceInM <= radiusInM) {
+              matchingDocs.push(doc);
             }
           }
-    
-    
-         
-          return matchingDocs
-    
-        }).then((matchingDocs)=>{
-          console.log(matchingDocs, "then")
-         const markerCoords = matchingDocs.map((docs)=>{
-           
-            const lat =  docs._delegate._document.data.value.mapValue.fields.lat.doubleValue
-            const long = docs._delegate._document.data.value.mapValue.fields.long.doubleValue
-            
-           return <Marker key={`${Math.random()}`} coordinate={{"latitude": lat, "longitude": long}} />
-    
-          })
-          console.log(markerCoords, "marker Array")
-          
-          setMarkerArray(markerCoords)
-    
-        }).catch((error)=>{
-          alert(error)
-        })
-    
-    
-      }
+        }
 
- useEffect(()=>{
-  retrieveMarkers()
-  
-    }, [])
+        return matchingDocs;
+      })
+      .then((matchingDocs) => {
+        console.log(matchingDocs, "then");
 
-      return{ markerArray, mapToggle, setMapToggle}
+        const deltaArray = matchingDocs.map((docs) => {
+          const lat =
+            docs._delegate._document.data.value.mapValue.fields.lat.doubleValue;
+          const long =
+            docs._delegate._document.data.value.mapValue.fields.long
+              .doubleValue;
+          return { latitude: lat, longitude: long };
+        });
+
+        function getRegionForCoordinates(points) {
+          // points should be an array of { latitude: X, longitude: Y }
+          let minX, maxX, minY, maxY;
+
+          // init first point
+          ((point) => {
+            minX = point.latitude;
+            maxX = point.latitude;
+            minY = point.longitude;
+            maxY = point.longitude;
+          })(points[0]);
+
+          // calculate rect
+          points.map((point) => {
+            minX = Math.min(minX, point.latitude);
+            maxX = Math.max(maxX, point.latitude);
+            minY = Math.min(minY, point.longitude);
+            maxY = Math.max(maxY, point.longitude);
+          });
+
+          const midX = (minX + maxX) / 2;
+          const midY = (minY + maxY) / 2;
+          const deltaX = maxX - minX;
+          const deltaY = maxY - minY;
+
+          return {
+            latitude: midX,
+            longitude: midY,
+            latitudeDelta: deltaX,
+            longitudeDelta: deltaY,
+          };
+        }
+        const deltaCoords = getRegionForCoordinates(deltaArray);
+        console.log(deltaCoords, "<------------------delta coords");
+
+        const markerCoords = matchingDocs.map((docs) => {
+          const lat =
+            docs._delegate._document.data.value.mapValue.fields.lat.doubleValue;
+          const long =
+            docs._delegate._document.data.value.mapValue.fields.long
+              .doubleValue;
+
+          return (
+            <Marker
+              key={`${Math.random()}`}
+              coordinate={{ latitude: lat, longitude: long }}
+            />
+          );
+        });
+        // console.log(markerCoords, "marker Array");
+
+        setMarkerArray(markerCoords);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    retrieveMarkers();
+  }, []);
+
+  return { markerArray, mapToggle, setMapToggle };
 }
--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet,  View, Dimensions, Text } from 'react-native';
-import useMarkers from '../../Hooks/useMarkers';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import RequestLocationData from '../../utils/RequestLocationData';
-
+import React, { useState } from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, View, Dimensions, Text } from "react-native";
+import useMarkers from "../../Hooks/useMarkers";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import RequestLocationData from "../../utils/RequestLocationData";
 
 export default function MapScreen() {
+  const { markerArray, mapToggle, setMapToggle } = useMarkers();
 
-    const {markerArray, mapToggle, setMapToggle} = useMarkers()
- 
-
-    console.log(markerArray, "marker in map")
-
-
- 
-
-  
-
-
+  console.log(markerArray, "marker in map");
+  const initRegion = {
+    latitude: 53.661119347171336,
+    latitudeDelta: 0.0003385718758650569,
+    longitude: -2.966440352731869,
+    longitudeDelta: 0.00039982805485072603,
+  };
 
   return (
     <View style={styles.container}>
-     
-     <MapView style={styles.map}>
+      <MapView style={styles.map} region={initRegion}>
         {markerArray}
-
-        </MapView>
-        <TouchableOpacity onPress={()=>{
-            RequestLocationData()
-        }}><Text>Set Location</Text></TouchableOpacity>
+      </MapView>
+      <TouchableOpacity
+        onPress={() => {
+          RequestLocationData();
+        }}
+      >
+        <Text>Set Location</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -37,12 +35,12 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
