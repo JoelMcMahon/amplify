7081470b95d2d0a9fc30de4542d38d64aecf74c1
emilyb93 <emilybennett93@hotmail.com>
2021-11-02 18:18:09 +0000

    fixed navigation bug by removing navigation.navigate

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import firebase from "firebase";
+import { userAppAuth } from "../../hooks/userAppAuth";
 
-const Profile = () => {
+const Profile = ({ setUser }) => {
+  const logoutHandler = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then((res) => {
+        console.log(res);
+
+        setUser(null);
+      })
+      .catch((err) => console.dir(err));
+  };
   return (
     <View>
       <Text>Profile</Text>
+      <Button
+        title="Log Out"
+        onPress={() => {
+          logoutHandler();
+        }}
+      />
     </View>
   );
 };
