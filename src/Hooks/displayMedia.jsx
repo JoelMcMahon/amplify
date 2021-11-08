f58e49802330a52bb5f50a1cdea4d3a7d16026c4
JoelM <joelmcmahon2@gmail.com>
2021-11-08 16:52:58 +0000

    add single ad screen

---
+++ src/Hooks/displayMedia.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Image, View, Text, StyleSheet } from "react-native";
+import { Video } from "expo-av";
+
+export const displayMedia = (type, uri) => {
+  console.log(uri);
+  if (type === "photo") {
+    return <Image style={styles.media} source={{ uri }} />;
+  } else if (type === "video") {
+    return (
+      <Video
+        style={styles.media}
+        source={{ uri }}
+        useNativeControls
+        isLooping
+      />
+    );
+  } else {
+    return (
+      <View style={styles.media}>
+        <Text>No Media</Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  media: {
+    height: 200,
+    width: 200,
+    // flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+});
--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Pressable, Text, View, Button } from "react-native";
 import { db } from "../../firebase/config";
-import { logoutHandler } from "../../utils/dbInteraction";
 import PostFeed from "../../utils/PostFeed";
-import style from "./style";
+import { createStackNavigator } from "@react-navigation/stack";
+import ProfilePage from "./ProfilePage";
+import SingleAd from "../SingleAd/SingleAd";
+
+const Stack = createStackNavigator();
 
 const Profile = ({ user, setUser }) => {
-  const { fullName, displayName, id, email } = user;
-
   const [showPosts, setShowPosts] = useState(false);
 
   const [posts, setPosts] = useState([]);
   const [order, setOrder] = useState("asc");
+  const [currentAd, setCurrentAd] = useState({});
 
   const toggleOrder = () => {
     setOrder(order === "asc" ? "desc" : "asc");
@@ -40,36 +41,57 @@ const Profile = ({ user, setUser }) => {
   };
 
   return (
-    <>
-      <Button title="Posts" onPress={togglePosts} />
-      {showPosts ? (
-        <>
-          <Button
-            title={order === "desc" ? "Oldest first" : "Latest first"}
-            onPress={toggleOrder}
+    <Stack.Navigator screenOptions={{ header: () => null }}>
+      <Stack.Screen name="ProfileInfo">
+        {(props) => <ProfilePage {...props} user={user} setUser={setUser} />}
+      </Stack.Screen>
+      <Stack.Screen name="ProfilePosts">
+        {(props) => (
+          <PostFeed {...props} ads={posts} setCurrentAd={setCurrentAd} />
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="SingleAd">
+        {(props) => (
+          <SingleAd
+            {...props}
+            ads={posts}
+            currentAd={currentAd}
+            onProfile={true}
           />
-          <PostFeed ads={posts} />
-        </>
-      ) : (
-        <>
-          <View style={style.container}>
-            <Text>User: {fullName}</Text>
-            <Text>Display Name: {displayName}</Text>
-            <Text>Email: {email}</Text>
-
-            <Pressable
-              onPress={() => {
-                logoutHandler(setUser);
-              }}
-              style={style.logout}
-            >
-              <Text style={style.buttonText}>Logout</Text>
-            </Pressable>
-          </View>
-        </>
-      )}
-    </>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
   );
+
+  // <>
+  //   <Button title="Posts" onPress={togglePosts} />
+  //   {showPosts ? (
+  //     <>
+  //       <Button
+  //         title={order === "desc" ? "Oldest first" : "Latest first"}
+  //         onPress={toggleOrder}
+  //       />
+  //       <PostFeed ads={posts} />
+  //     </>
+  //   ) : (
+  //     <>
+  //       <View style={style.container}>
+  //         <Text>User: {fullName}</Text>
+  //         <Text>Display Name: {displayName}</Text>
+  //         <Text>Email: {email}</Text>
+
+  //         <Pressable
+  //           onPress={() => {
+  //             logoutHandler(setUser);
+  //           }}
+  //           style={style.logout}
+  //         >
+  //           <Text style={style.buttonText}>Logout</Text>
+  //         </Pressable>
+  //       </View>
+  //     </>
+  //   )}
+  // </>
 };
 
 export default Profile;
