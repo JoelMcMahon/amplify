40a26fbb1e0941122d196bf73f3e6c55197d7a6f
CtrlHoltDel <ctrlholtdel@gmail.com>
2021-11-10 22:28:18 +0000

    Removed border from bottom nav

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,45 +9,17 @@ import { userAppAuth } from "./src/Hooks/userAppAuth";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "./src/screens/Profile/Profile";
 import { navIcons } from "./src/utils/navIcons";
-import useMessages from "./src/Hooks/useMessages";
-import useChats from "./src/Hooks/useChats";
 import NewPostNav from "./src/screens/NewPost/NewPost";
 import Inbox from "./src/screens/InboxScreen/InboxScreen";
-import fetchUsers from "./src/Hooks/fetchUsers";
 import { testChat } from "./src/Hooks/testChats";
-=======
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
-import { decode, encode } from 'base-64';
-import { LogBox } from 'react-native';
-import { userAppAuth } from './src/Hooks/userAppAuth';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Profile from './src/screens/Profile/Profile';
-import { navIcons } from './src/utils/navIcons';
-import NewPostNav from './src/screens/NewPost/NewPost';
-import Inbox from './src/screens/InboxScreen/InboxScreen';
-import { testChat } from './src/Hooks/testChats';
->>>>>>> 694f90928cd08e7b1cff23fc4770cf78bf12bd89
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
 const theme = {
   ...DefaultTheme,
-<<<<<<< HEAD
-  dark:true,
-  roundness: 50,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#fc7f19',
-    accent: '#fc7f19',
-=======
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#e05c10',
-    text: 'white',
->>>>>>> 694f90928cd08e7b1cff23fc4770cf78bf12bd89
+    primary: "#e05c10",
+    text: "white",
   },
 };
 
@@ -59,8 +30,8 @@ if (!global.atob) {
   global.atob = decode;
 }
 
-LogBox.ignoreLogs(['Setting a timer']);
-LogBox.ignoreLogs(['Async Storage has been extracted from react-native core']);
+LogBox.ignoreLogs(["Setting a timer"]);
+LogBox.ignoreLogs(["Async Storage has been extracted from react-native core"]);
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -129,17 +100,10 @@ export default function App() {
   };
 
   return (
-<<<<<<< HEAD
-    <PaperProvider  theme={theme}>
-    <NavigationContainer>
-      {isLoggedIn ? tabs() : loginSignup()}
-    </NavigationContainer>
-=======
     <PaperProvider theme={theme}>
       <NavigationContainer>
         {isLoggedIn ? tabs() : loginSignup()}
       </NavigationContainer>
->>>>>>> 694f90928cd08e7b1cff23fc4770cf78bf12bd89
     </PaperProvider>
   );
 }
