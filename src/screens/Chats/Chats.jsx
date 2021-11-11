c86a1ef6fc3585fd4d8ae28c896f9e1bac0c6142
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-11-11 12:19:18 +0000

    Add profile pictures to chats in inbox

--- src/screens/Chats/Chats.jsx
+++ src/screens/Chats/Chats.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
-import { Text, StyleSheet } from "react-native";
-import styles from "./styles";
-import { Card, Subheading, Title } from "react-native-paper";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import { SearchBar } from "react-native-elements";
-import fetchUsers from "../../Hooks/fetchUsers";
-import createChatRoom from "../../utils/createChatRoom";
-import getSingleChat from "../../utils/getSingleChat";
-import { LinearGradient } from "expo-linear-gradient";
-import { Entypo } from "@expo/vector-icons";
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+import { Card, Title } from 'react-native-paper';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { SearchBar } from 'react-native-elements';
+import fetchUsers from '../../Hooks/fetchUsers';
+import createChatRoom from '../../utils/createChatRoom';
+import getSingleChat from '../../utils/getSingleChat';
+import { Avatar, Divider } from 'react-native-paper';
+import { Entypo } from '@expo/vector-icons';
 
 const Chats = ({ navigation, chatArray, usersArray, currUser }) => {
   const { searchStr, setSearchStr } = fetchUsers();
   const [tempMessages, setTempMessages] = useState([]);
-  // console.log(chatArray, "<<<<<<<< chat array for ", currUser.displayName);
+  const { displayName } = currUser;
+
+  const imgs = {
+    CelinaAd: require('../../../assets/pexels-anna-tarazevich-8479248.jpg'),
+    rebelJeff: require('../../../assets/pexels-amine-msiouri-2108813.jpg'),
+    Brent123: require('../../../assets/pexels-rodolfo-quirós-1727280.jpg'),
+    Brentley86: require('../../../assets/pexels-cottonbro-6503569.jpg'),
+    BrendanIam: require('../../../assets/pexels-wendy-wei-1576280.jpg'),
+    BrentwoodH: require('../../../assets/pexels-jack-carey-3331904.jpg'),
+    McLarry: require('../../../assets/pexels-rodolfo-quirós-1727280.jpg'),
+    Ctrlholtdel: require('../../../assets/pexels-nichole-sebastian-3361381.jpg'),
+    dave334: require('../../../assets/pexels-anne-mccarthy-6344364.jpg'),
+    Emilyb93: require('../../../assets/pexels-cottonbro-6853299.jpg'),
+    steveRaw: require('../../../assets/pexels-cottonbro-6503569.jpg'),
+  };
+
   return (
     <ScrollView style={styles.background}>
       <SearchBar
@@ -30,6 +45,7 @@ const Chats = ({ navigation, chatArray, usersArray, currUser }) => {
               <Card key={user.id}>
                 <Card.Content style={styles.userCard}>
                   <TouchableOpacity
+                    style={styles.chatcontainer}
                     onPress={async () => {
                       const newRoomId = await createChatRoom(
                         currUser.id,
@@ -39,20 +55,28 @@ const Chats = ({ navigation, chatArray, usersArray, currUser }) => {
                         chatArray
                       );
                       const messages = await getSingleChat(newRoomId, currUser);
-                      // console.log(newRoomId, "<newId");
                       navigation.navigate({
-                        name: "SingleChat",
+                        name: 'SingleChat',
                         params: { roomId: newRoomId, messages },
                       });
-                      setSearchStr("");
+                      setSearchStr('');
                     }}
                   >
-                    <Title>
+                    <Title style={styles.searchtitle}>
                       <Entypo name="new-message" size={16} color="#FEF4EC" />
-                      {"  "}
-                      {user.displayName}
+                      <Text style={styles.text}>
+                        {' '}
+                        {`  ${user.displayName}`}{' '}
+                      </Text>
                     </Title>
+
+                    <Avatar.Image
+                      style={styles.avatar}
+                      size={45}
+                      source={imgs[user.displayName]}
+                    />
                   </TouchableOpacity>
+                  <Divider style={styles.divider} />
                 </Card.Content>
               </Card>
             );
@@ -62,18 +86,28 @@ const Chats = ({ navigation, chatArray, usersArray, currUser }) => {
               <Card key={roomId.id} style={styles.card}>
                 <Card.Content>
                   <TouchableOpacity
+                    style={styles.chatcontainer}
                     onPress={async () => {
                       const messages = await getSingleChat(roomId.id, currUser);
 
                       navigation.navigate({
-                        name: "SingleChat",
+                        name: 'SingleChat',
                         params: { roomId: roomId.id, messages },
                       });
                     }}
                   >
+                    <Avatar.Image
+                      style={styles.avatar}
+                      size={45}
+                      source={
+                        roomId.displayNames[0] === currUser.displayName
+                          ? imgs[roomId.displayNames[1]]
+                          : imgs[currUser.displayName]
+                      }
+                    />
                     <Title>
                       <Text styles={styles.text}>
-                        {" "}
+                        {' '}
                         {roomId.displayNames[0] === currUser.displayName
                           ? roomId.displayNames[1]
                           : roomId.displayNames[0]}
